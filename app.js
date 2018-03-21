49cb7d44d4dc0d6f30f180ee3fc95003182f77d2
mike crane <mike.c0321@gmail.com>
2018-03-21 12:41:24 -0400

    added starter HTML and function stubs

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function getApiData() {
+  // pass url endpoint, query object, and callback function
+}
+
+function renderResult() {
+  // appends some API data to the html results section.
+}
+
+function handleSubmitButton() {
+  // on submit, send search query to function responsible for API call
+}
+
+$(handleSubmitButton);
